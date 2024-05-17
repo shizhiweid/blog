9d41d64c18fc234e9625062d079abe15caf7eaee
shi <1613285977@qq.com>
2024-05-17 16:00:29 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,6 @@ set -e
 # 生成静态文件
 npm run docs:build
 
-# 进入生成的文件夹
-cd docs/.vuepress/dist
-
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 git add .
@@ -19,5 +16,3 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push
-
-cd -
